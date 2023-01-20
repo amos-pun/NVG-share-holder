9af793c3db29b9d490598c680bdd89464f2df484
Your Name <amos.pun1994@gmail.com>
2023-01-21 00:58:40 +0545

    Nvg

--- src/Card.js
+++ src/Card.js
@@ -3,9 +3,9 @@ import React from "react";
 const Card = ({name, email , id, Username, img}) => {
     return(
         <div className="card bg-secondary m-2 col-4" style={{width:'16rem'}}>
-                <img src={img} alt="img." height="200px" className="text-center"/>
+                <img src={img} alt="img." height="200px" className="text-center m-1"/>
             <div>
-                <h2>{name}</h2>
+                <h3>{name}</h3>
                 <h4>{Username}</h4>
                 <p>{email}</p>
 
