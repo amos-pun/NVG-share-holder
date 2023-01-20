975f88d8c8377077e3fdff0c5d99c2d7bd584f57
Your Name <amos.pun1994@gmail.com>
2023-01-21 00:43:37 +0545

    Photo added

--- src/robots.js
+++ src/robots.js
@@ -20,38 +20,50 @@ export const robots = [
    },
    {
     name:'Narayan Tamang',
+    img:'./img/Nara.jpg'
    },
    {
     name:'Sagar Budha',
+    img:'./img/sagar.jpg'
    },
    {
     name:'Asmita Gubhaju',
+    img:'./img/asmi.jpg'
    },
    {
     name:'Dipesh Pun',
+    img:'./img/dipesh.jpg'
    },
    {
     name:'Patam Purja',
+    img:'./img/patam.jpg'
    },
    {
     name:'Sanubabu Shrestha',
+    img:'./img/solomon.jpg'
    },
    {
     name:'Asap Pun',
+    img:'./img/.jpg'
    },
    {
-    name:'Khim Kumari Pun',
+    name:'Khim Kumar Pun',
+    img:'./img/khim.jpg'
    },
    {
     name:'Adam Pun',
+    img:'./img/adam.jpg'
    },
    {
     name:'Santosh Bajgain',
+    img:'./img/santosh.jpg'
    },
    {
     name:'Sion Rajali Magar',
+    img:'./img/sion.jpg'
    },
    {
     name:'Bijay Chamling Rai',
+    img:'./img/bj.jpg'
    },
 ]
